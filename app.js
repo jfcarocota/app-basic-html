860285ad75e1afc2ad028347029c166d9ab50bf2
jfcarocota <jfcarocota@gmail.com>
2019-08-27 12:40:02 -0700

    piano working

--- app.js
+++ app.js
@@ -2,7 +2,15 @@
 const buttons = document.querySelectorAll('button');
 
 const playnote = event =>{
-    console.log(event.target.dataset.note);
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+    //console.log(audioId);
+    const audio = document.getElementById(audioId);
+    console.log(audio);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 }
 
 buttons.forEach(
