6babe344273f625445cd63808f7f99728c5a3ec2
jfcarocota <jfcarocota@gmail.com>
2019-08-27 12:00:03 -0700

    tecla pulsada

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+const buttons = document.querySelectorAll('button');
+
+const playnote = event =>{
+    console.log(event.target.dataset.note);
+}
+
+buttons.forEach(
+    button => button.addEventListener('click', playnote)  
+);
